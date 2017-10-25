9386c571fe0e29e466b3f9fc5d70673c9454805e
liviaaloise <livia.aloise@gmail.com>
2017-10-25 19:58:31 -0200

    Add files via upload

--- escalonador.c
+++ escalonador.c
@@ -18,7 +18,9 @@ void fimHandler(int signal);
 void exibeProcHandler(int signal);
 void RoundRobin(Fila *f,int t);
 void IOHandler(int signal);
-//int execProc(Proc *p, int t);
+void CHLDHandler(int signal);
+resultEx execProc(Proc *p, int t);
+int indice_Rajada (Proc *p);
 
 int main (int argc, char** argv){
    	
@@ -28,6 +30,8 @@ int main (int argc, char** argv){
 
     signal(SIGUSR1,procHandler);
 	signal(SIGQUIT,fimHandler);
+	signal(SIGCHLD,CHLDHandler);
+	signal(SIGUSR2,IOHandler);
 	//signal(SIGUSR2,exibeProcHandler);
 	
 	
@@ -41,28 +45,6 @@ int main (int argc, char** argv){
 	return 0;	
 }
 
-void  procHandler(int signal){
-	Proc *p,*pNovo;
-	int seg1,i,pid;
-	seg1 = shmget(key,sizeof(Proc),IPC_EXCL| S_IRUSR | S_IWUSR);
-	p=(Proc*)shmat(seg1,0,0);
-	
-	pNovo=(Proc*)malloc(sizeof(Proc));
-	
-	pNovo->fila=p->fila;
-	pNovo->estado=p->estado;
-	pNovo->pid=p->pid;
-	strcpy(pNovo->nome,p->nome);
-	pNovo->numR=p->numR;
-	
-	 for(i=0;i<p->numR;i++){
-        pNovo->raj[i]=p->raj[i];
-    }
-	
-	fila_push(fila1,p);
-	return;
-	
-}
 
 int verificaRR(int t){
 	int i;
@@ -96,13 +78,13 @@ void RoundRobin(Fila *f){
 			p=fila_pop(f);
 			cond=execProc(p,t);
 			
-			if(ret==0)//TERMINOU
+			if(cond==0)//TERMINOU
 			{
 				printf("Processo terminou \n");
 				free(p);
 			}
 			
-			else if(cond==1)//Nao terminou e acabou o quantum
+			else if(cond==1)//FALTOU TEMPO NA RAJADA
 			{
 				if(t==1){
 					p->fila=2;
@@ -114,30 +96,79 @@ void RoundRobin(Fila *f){
 				}
 			}
 			
-			else{//SOBROU TEMPO
-				kill()
+			else{//SOBROU TEMPO NA RAJADA
 				if(t==1 || t==2)
 				{
 					p->fila=1;		
-					fila_push(fila1,p);
 				}
 				else{
 					p->fila=2;
-					fila_push(fila2,p);
-				}	
+				}
 				
+				fila_push(filaIO,p);	
+				kill(SIGUSR2,IOHandler);
 			}			
 			
 	}
 	return;	
 }
 
-void IOHandler(int signal){
-	
+resultEx execProc(Proc *p, int t){
+	char cmdC[MAX]="./" ;
+	int indR;
+	if (p->estado==novo){ //processo novo -> criar processo filho
+		p->pid=fork();
+		if(p->pid==0){ //processo filho
+			strcat(cmdC,p->nome);
+			kill(getpid(),SIGSTOP);
+			execv(cmdC,NULL);
+		}
+	}
+	indR = indice_Rajada(p);
+	if(t >= p->raj[indR]){ //se eu tenho >= tempo pra executar do que a rajada que o processo ta
+		kill(p->pid,SIGINT);
+		sleep(p->raj[indR]); //executa durante esse tempo
+		kill(p->pid,SIGSTOP);
+		p->raj[indR]=0; //zera o tempo da rajada
+		if(indice_Rajada(p)==-1){//se nao tem mais rajada diferente de 0
+			p->estado=terminado;
+			return terminou;
+		}
+		else{ // tem mais rajada-> entra em espera de io
+			p->estado=io;
+			return sobrouTempo;
+		}
+	}
+	if(t < p->raj[indR]){//menos tempo do q precisa
+		kill(p->pid,SIGINT);
+		sleep(t); //executa durante t
+		kill(p->pid,SIGSTOP);
+		p->raj[indR]-=t;
+		p->estado=fila;
+		return faltouTempo;
+	}
 }
 
+int indice_Rajada (Proc *p){
+	int i;
+	for(i=0;i<p->numR;i++)
+		if(p->raj[i]!=0)
+			return i;
+	return -1; 
+}
+
+
+
+
 void CHLDHandler(int signal){
-	
+	int pidE= waitpid(-1, NULL, WNOHANG);
+	if (pidE==0)//Processo parou por um sinal
+	{
+		return;//Nao faz nada;
+	}
+	else{
+		printf("Processo %d terminou\n",pidE);
+	}	
 }
 
 
@@ -157,6 +188,7 @@ void exibeProcHandler(int signal){
         printf("v[%d]=%d\n", i,p->raj[i]);
     }
 }
+
 
 void fimHandler(int signal){
 	int seg1;
