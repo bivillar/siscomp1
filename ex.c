753c87c1e4e334d579466a206a8305c9ea8f9e12
liviaaloise <livia.aloise@gmail.com>
2017-10-25 20:44:21 -0200

    Add files via upload

--- ex.c
+++ ex.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-
 int main(){
-	int status;
-    char *const arg[]={"escalonador.c",NULL};
-	printf("exercicio 4 \n");
-	
-	if(fork()!=0){/* Comandos Pai */
-		waitpid( -1, &status, 0);
-	}
-	else{ /*Comandos Filho*/
-		execv("./e", arg);	
-	}
+   int i;
+   for(i=0;i<2;i++){
+   		printf("pid: %d\n",getpid());
+   		sleep(1);
+   }
+   for(i=0;i<3;i++){
+   		printf("pid: %d\n",getpid());
+   		sleep(1);
+   }
 	return 0;
 } 
